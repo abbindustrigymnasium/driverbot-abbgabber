98fae2c17cea011522647ae2b2ce9665069ba796
abbgabber <gabriel.bergdahl@abbindustrigymnasium.se>
2020-03-31 14:03:34 +0200

    Added Vue

---
+++ driverbot/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-btn @click="sendPayload()">
+      Click me!
+    </v-btn>
+  </v-container>
+</template>
+
+<script>
+const mqtt = require("mqtt");
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    client: "",
+    payload: "YEs"
+  }),
+
+  mounted: function() {
+    this.client = mqtt.connect("ws://maqiatto.com:8883/ws", {
+      username: "gabriel.bergdahl@abbindustrigymnasium.se",
+      password: "qazwsxedc"
+    });
+  },
+  methods: {
+    sendPayload() {
+      this.client.publish(
+        "gabriel.bergdahl@abbindustrigymnasium.se/driverbot",
+        this.payload
+      );
+      console.log(this.payload);
+    }
+  }
+};
+</script>
