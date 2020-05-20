f24eca16ed62a3f3c05e7e9b4be0012fca267c31
abbgabber <gabriel.bergdahl@abbindustrigymnasium.se>
2020-05-20 11:44:45 +0200

    Update HelloWorld.vue

--- driverbot/src/components/HelloWorld.vue
+++ driverbot/src/components/HelloWorld.vue
@@ -1,8 +1,18 @@
 <template>
   <v-container>
-    <v-btn @click="sendPayload()">
-      Click me!
-    </v-btn>
+    <v-slider v-model="onoff" min="0" max="1" label="On / Off"> </v-slider>
+    <v-slider
+      v-model="velocity"
+      min="-100"
+      max="100"
+      label="Hastighet"
+    ></v-slider>
+    <v-slider
+      v-model="direction"
+      min="-90"
+      max="90"
+      label="Riktning"
+    ></v-slider>
   </v-container>
 </template>
 
@@ -13,23 +23,34 @@ export default {
 
   data: () => ({
     client: "",
-    payload: "YEs"
+    payload: "YEs",
+    velocity: "0",
+    direction: "0",
+    onoff: "1",
   }),
 
-  mounted: function() {
+  created() {
     this.client = mqtt.connect("ws://maqiatto.com:8883/ws", {
       username: "gabriel.bergdahl@abbindustrigymnasium.se",
-      password: "qazwsxedc"
+      password: "qazwsxedc",
     });
+  },
+  mounted() {
+    setInterval(() => {
+      this.sendPayload();
+    }, 2000);
   },
   methods: {
     sendPayload() {
-      this.client.publish(
-        "gabriel.bergdahl@abbindustrigymnasium.se/driverbot",
-        this.payload
-      );
-      console.log(this.payload);
-    }
-  }
+      if (this.onoff !== 0) {
+        if (this.direction !== 0 || this.velocity !== 0) {
+          this.client.publish(
+            "gabriel.bergdahl@abbindustrigymnasium.se/driverbot",
+            this.velocity.toString() + " " + this.direction.toString()
+          );
+        }
+      }
+    },
+  },
 };
 </script>
