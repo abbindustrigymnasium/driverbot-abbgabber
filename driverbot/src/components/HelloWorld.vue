45ed1ce51c20afb4a8506a6ea1380433097c3231
abbgabber <gabriel.bergdahl@abbindustrigymnasium.se>
2020-06-01 15:37:06 +0200

    Commented

--- driverbot/src/components/HelloWorld.vue
+++ driverbot/src/components/HelloWorld.vue
@@ -22,6 +22,7 @@ export default {
     onoff: "0",
   }),
 
+  //Connects to mqtt
   created() {
     this.client = mqtt.connect("ws://maqiatto.com:8883/ws", {
       username: "gabriel.bergdahl@abbindustrigymnasium.se",
@@ -34,19 +35,24 @@ export default {
     }, 1000);
   },
   methods: {
+    // Sends the payload every 1/10 second
     sendPayload() {
       if (this.onoff !== 0) {
+        // Uses the velocity from the slider as value and repeats 0s to make it easier to substring
         let fast =
           "0".repeat(4 - this.velocity.toString().length) +
           this.velocity.toString();
+        // Uses the angle from the slider as value and repeats 0s to make it easier to substring
         let angles =
           "0".repeat(4 - this.direction.toString().length) +
           this.direction.toString();
+        // Publishes the full message with both values
         this.client.publish(
           "gabriel.bergdahl@abbindustrigymnasium.se/driverbot",
           fast.toString() + angles.toString()
         );
       } else {
+        // When pressing off resets the values to standard, 0 speed and angle.
         this.client.publish(
           "gabriel.bergdahl@abbindustrigymnasium.se/driverbot",
           "01000090"
